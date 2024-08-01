18e8364c0620c7b2babacad540c668b87877d1f0
Daniel Bird <danielbirdpersonal@gmail.com>
2024-08-01 00:35:28 -0400

    Little details

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -10,9 +10,12 @@ const Dashboard = () => {
                 <p>days out</p>
             </div>
             <div className='checkin-widget widget'>
-                <h3>Sleep 7:45 hr</h3>
-                <h3>Stress 2/5</h3>
-                <h3>Weight -0.48 lb</h3>
+                <h3>Sleep</h3>
+                <h3>7:45 hr</h3>
+                <h3>Stress</h3>
+                <h3>2/5</h3>
+                <h3>Weight</h3>
+                <h3>-0.48 lb</h3>
             </div>
             <div className='vol-int-graph-widget widget'>
                 <div className='mesocycle-title'>
@@ -20,11 +23,10 @@ const Dashboard = () => {
                     <h5>JUL 15 - OCT 29</h5>
                 </div>
                 <div className='widget-line'></div>
-                    <div>Contents of graph would go here</div>
+                    <div>The contents of graph would go here. This is a graph that would display the volume and intensity progress from week to week throughout this mesocycle. For example, it would avg the total set weekly volume & total set RPE for each week of the meso.</div>
             </div>
             <div className='week-overview-widget widget'>
-                <div className='week-overview-title '>
-                    <h5>Overview</h5>
+                <div className='week-overview-title'>
                     <h5>Week 1</h5>
                     <div className='week-navigation-arrows'>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className="left-chevron-icon"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
@@ -32,26 +34,26 @@ const Dashboard = () => {
                     </div>
                 </div>
                 <div className='week-overview-day'>
-                    <h6>Day 1</h6>
+                    <h5>Day 1</h5>
                 </div>
                 <div className='week-overview-day'>
-                    <h6>Day 2</h6>
+                    <h5>Day 2</h5>
                 </div>
                 <div className='week-overview-day'>
-                    <h6>Day 3</h6>
+                    <h5>Day 3</h5>
                 </div>
                 <div className='week-overview-day'>
-                    <h6>Day 4</h6>
+                    <h5>Day 4</h5>
                 </div>
                 <div className='week-overview-day'>
-                    <h6>Day 5</h6>
+                    <h5>Day 5</h5>
                 </div>
                 <div className='week-overview-day'>
-                    <h6>Day 6</h6>
+                    <h5>Day 6</h5>
                 </div>
                 <button className='week-overview-day complete-week'>
-                    <h6>Complete Week</h6>
-                    <h6>1/6</h6>
+                    <h5>Complete Week</h5>
+                    <h5>0/6</h5>
                 </button>
             </div>
         </div>
