a144f05eb6854450fbca377ec63abcb0cf7360ce
Daniel Bird <danielbirdpersonal@gmail.com>
2024-08-31 10:28:35 -0400

    Added features to pricing cards

--- src/pages/pricing.jsx
+++ src/pages/pricing.jsx
@@ -11,50 +11,46 @@ const Pricing = () => {
                     <div className="price_card">
                         <h2>Basic</h2>
                         <div className="price_discount">
-                            <p className="price">$9</p>
+                            <p className="price original_price">$9</p>
+                            <p className="price">Free</p>
                         </div>
                         <small className="per_month">per month</small>
                         <div className="price_card_line"></div>
                         <ul className="price_feature_list">
-                            <li className="price_feature">✓ Feature</li>
-                            <li className="price_feature">✓ Feature</li>
-                            <li className="last_price_feature price_feature">✓ Feature</li>
+                            <li className="price_feature">✓&nbsp; Track workouts & see progress</li>
+                            <li className="price_feature">✓&nbsp; Create custom programs</li>
+                            <li className="price_feature">✓&nbsp; Weight, reps, and RPE entry</li>
+                            <li className="last_price_feature price_feature">✓&nbsp; Use calculations in program</li>
                         </ul>
-                        <button>Get Started</button>
+                        <button onClick={() => window.location.href='/sign-up'}>Get Started</button>
                     </div>
                     <div className="price_card" id="price_card_2">
-                        <h2>Popular</h2>
+                        <h2>Pro</h2>
                         <div className="price_discount">
-                            <p className="price original_price">$60</p>
-                            <p className="price">$19</p>
+                            {/* <p className="price original_price">$60</p> */}
+                            <p className="price">Coming Soon</p>
                         </div>
                         <small className="per_month">per month</small>
                         <div className="price_card_line"></div>
                         <ul className="price_feature_list">
-                            <li className="price_feature">✓ Feature</li>
-                            <li className="price_feature">✓ Feature</li>
-                            <li className="price_feature">✓ Feature</li>
-                            <li className="price_feature">✓ Feature</li>
-                            <li className="last_price_feature price_feature">✓ Feature</li>
+                            <li className="price_feature">✓&nbsp; All Basic features</li>
+                            <li className="price_feature">✓&nbsp; AI-based progress optimization</li>
+                            <li className="last_price_feature price_feature">✓&nbsp; Store videos & progress photos</li>
                         </ul>
-                        <button>Get Started</button>
+                        <button onClick={() => alert('Coming Soon!')}>Coming Soon</button>
                     </div>
                     <div className="price_card">
                         <h2>Premium</h2>
                         <div className="price_discount">
-                            <p className="price">$79</p>
+                            <p className="price">Coming Soon</p>
                         </div>
                         <small className="per_month">per month</small>
                         <div className="price_card_line"></div>
                         <ul className="price_feature_list">
-                            <li className="price_feature">✓ Feature</li>
-                            <li className="price_feature">✓ Feature</li>
-                            <li className="price_feature">✓ Feature</li>
-                            <li className="price_feature">✓ Feature</li>
-                            <li className="price_feature">✓ Feature</li>
-                            <li className="last_price_feature price_feature">✓ Feature</li>
+                            <li className="price_feature">✓&nbsp; All Pro features</li>
+                            <li className="last_price_feature price_feature">✓&nbsp; Coaches can view clients progress & manage programs</li>
                         </ul>
-                        <button>Get Started</button>
+                        <button >Coming Soon</button>
                     </div>
                 </div>
             </section>
