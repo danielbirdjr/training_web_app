c8296cb8889b666fc4cadc9654e39df977b01ce4
Daniel Bird <danielbirdpersonal@gmail.com>
2024-09-14 15:40:12 -0400

    Added top bar on custom page

--- src/pages/program/custom.jsx
+++ src/pages/program/custom.jsx
@@ -2,30 +2,56 @@ import React, { useState } from 'react';
 import withAuth from '../../hoc/withAuth';
 import DayComponent from '../../components/DayComponent';
 import AddDayButtonComponent from '../../components/AddDayButtonComponent';
+import { FiMoreHorizontal } from "react-icons/fi";
 import '../../styles/program/custom.css';
 
 const CustomProgram = () => {
-  const [days, setDays] = useState([{}]); // Start with two days by default
+    const [days, setDays] = useState([{}]); // Start with two days by default
+    const [programTitle, setProgramTitle] = useState("Untitled program");
 
-  const handleAddDay = () => {
-    setDays([...days, {}]);
-  };
+    const handleAddDay = () => {
+        setDays([...days, {}]);
+    };
 
-  const handleDeleteDay = (index) => {
-    const updatedDays = days.filter((_, i) => i !== index);
-    setDays(updatedDays);
-  };
+    const handleDeleteDay = (index) => {
+        const updatedDays = days.filter((_, i) => i !== index);
+        setDays(updatedDays); 
+    };
 
-  return (
-    <div className="custom-program-creation-page">
-      <div className="days-container">
-        {days.map((_, index) => (
-          <DayComponent key={index} onDelete={() => handleDeleteDay(index)} />
-        ))}
-        <AddDayButtonComponent onAddDay={handleAddDay} />
-      </div>
-    </div>
-  );
+    const handleTitleChange = (e) => setProgramTitle(e.target.value);
+
+    const handleCreateOrSave = () => {
+        // Functionality to save the program or proceed to the next step
+        console.log("Program Saved:", programTitle);
+    };
+
+    return (
+        <>
+            <div className="top-bar">
+                <input 
+                    className="program-title-input" 
+                    value={programTitle} 
+                    onChange={handleTitleChange}
+                />
+                <div className='top-bar-button-and-options'>
+                    <button onClick={handleCreateOrSave} className="create-button">Create program</button>
+                    <button className="options-button">
+                        <FiMoreHorizontal />
+                    </button>
+                </div>
+                
+            </div>
+
+            <div className="custom-program-creation-page">
+                <div className="days-container">
+                    {days.map((_, index) => (
+                    <DayComponent key={index} onDelete={() => handleDeleteDay(index)} />
+                    ))}
+                    <AddDayButtonComponent onAddDay={handleAddDay} />
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default withAuth(CustomProgram);
